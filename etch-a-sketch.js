b4d56962336878d704c21294eb69ced622db85a8
Gavin Levis <levis.gavin@gmail.com>
2024-05-01 13:21:34 -0500

    add etch-a-sketch bg

---
+++ etch-a-sketch.js
@@ -0,0 +1,13 @@
+let sketch_container = document.querySelector('.container');
+let row = sketch_container.appendChild(document.createElement('div'));
+row.setAttribute('class', 'row');
+
+for(let i = 0; i < 16; i++) {
+    for(let j = 0; j < 16; j++) {
+        let div = row.appendChild(document.createElement('div'));
+        div.setAttribute('id', (i * 16) + j);
+        div.setAttribute('class', 'sketch-grid-item');
+    }
+    row = sketch_container.appendChild(document.createElement('div'));
+    row.setAttribute('class', 'row');
+}
