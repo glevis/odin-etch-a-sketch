6e9a9594d1d2a89758af1746d6d3070266a5690f
Gavin Levis <levis.gavin@gmail.com>
2024-05-01 21:59:01 -0500

    add resize button

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,16 +1,35 @@
-let sketch_container = document.querySelector('.container');
-let row = sketch_container.appendChild(document.createElement('div'));
-row.setAttribute('class', 'row');
+function generateBoard(size) {
+    let sketch_container = document.querySelector('.container');
+    sketch_container.innerHTML = '';
 
-for(let i = 0; i < 16; i++) {
-    for(let j = 0; j < 16; j++) {
-        let div = row.appendChild(document.createElement('div'));
-        div.setAttribute('id', (i * 16) + j);
-        div.setAttribute('class', 'sketch-grid-item');
-        div.addEventListener("mouseover", () => {
-            div.style.background = "black";
-        });
-    }
-    row = sketch_container.appendChild(document.createElement('div'));
+    let row = sketch_container.appendChild(document.createElement('div'));
     row.setAttribute('class', 'row');
+
+    for(let i = 0; i < size; i++) {
+        for(let j = 0; j < size; j++) {
+            let div = row.appendChild(document.createElement('div'));
+            div.setAttribute('id', (i * 16) + j);
+            div.setAttribute('class', 'sketch-grid-item');
+            div.addEventListener("mouseover", () => {
+                div.style.background = "black";
+            });
+        }
+        row = sketch_container.appendChild(document.createElement('div'));
+        row.setAttribute('class', 'row');
+    }
 }
+
+function getDimension() {
+    let dimension = prompt("Enter size:");
+    if(dimension > 100) {
+        alert("Limiting to 100 to save resources.");
+        dimension = 100;
+    }
+    return dimension;
+}
+
+const button = document.querySelector("#newGridButton");
+button.addEventListener('click', () => {
+    const dimension = getDimension();
+    generateBoard(dimension);
+})
